799f1e743c85ca977e145a3277285fff71fd534c
BogatinAfik <73298200+BogatinAfik@users.noreply.github.com>
2021-07-13 16:56:08 +0300

    Delete web-g15 directory

--- web-g15/pages/contact/static/js/contactpageJS.js
+++
@@ -1,9 +0,0 @@
-function checkEmail() {
-    var email = document.getElementById("email").value;
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email))
-  {
-    return (true)
-  }
-    alert("You have entered an invalid email address!")
-    return (false)
-}
--- web-g15/pages/registration/static/js/registrationJS.js
+++
@@ -1,7 +0,0 @@
-function checkPassword() { 
-    var pass = document.getElementById("psw").value;
-    var pass2 = document.getElementById("psw-repeat").value;
-     if (pass != pass2){  
-        alert ("Passwords do not match! Try again :( ") 
-     }
-    } 
